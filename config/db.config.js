b79c78a18173435a75412b845891d704b270d02d
amitmanore <amitdilip.m@inncircles.com>
2025-03-25 00:07:05 +0530

    commit 2

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://amitmanore691:amitmanore691@cluster0.i8xlxzk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        await mongoose.connect('mongodb+srv://amitmanore691:amit691@cluster0.rftym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
